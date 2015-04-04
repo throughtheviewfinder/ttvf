3a1c3f3a1fec5b98dbbb1ee6f7121734d27e6cef
janvt <janvt@me.com>
2015-04-04 14:52:05 +0200

    initial version

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+    <title>Through the Viewfinder</title>
+
+    <link rel="stylesheet" href="styles.css"/>
+</head>
+
+<body>
+    <div class="header">
+        <div class="logo">
+            LL
+        </div>
+
+        <div class="navigation">
+            <a href="contact.html">Contact</a>
+            <a href="about.html">About</a>
+        </div>
+
+    </div>
+
+    <div class="slideshow"></div>
+
+    <div class="footer">
+        <ul class="tiles">
+            <li>
+                <img src="./images/collections/15683460864_9091c86743_q.jpg"/>
+                <p class="title">concert</p>
+            </li>
+            <li>
+                <img src="./images/collections/16133722758_53ccf0279f_q.jpg"/>
+                <p class="title">fashion</p>
+            </li>
+            <li>
+                <img src="./images/collections/16892810316_0eaa62f27d_q.jpg"/>
+                <p class="title">studio</p>
+            </li>
+        </ul>
+    </div>
+
+</body>
+</html>
