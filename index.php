2a00b48123f4ec143f263ea5344870d3904afa96
janvt <janvt@me.com>
2015-04-04 19:31:45 +0200

    add slideshow

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
+<?php
+$images = [
+    "images/slideshow/16354034274_d141d2be06_k.jpg",
+    "images/slideshow/16835977820_306590d775_k.jpg",
+    "images/slideshow/17007939581_1bce7b0050_k.jpg",
+    "images/slideshow/16835781358_2243f027ef_k.jpg",
+    "images/slideshow/16261971583_a7b45b934b_k.jpg"
+];
+
+$slideShowCSS = '';
+for ($i = 1;$i < 6;$i++) {
+    $slideShowCSS .= '.gallery-image-' . $i . ' { background-image: url("' . $images[$i - 1] . '"); }' . "\n";
+}
+
+?>
 <html>
 <head>
     <title>Through the Viewfinder</title>
 
     <link rel="stylesheet" href="styles.css"/>
+    <link rel="stylesheet" href="gallery.css"/>
 </head>
 
 <body>
@@ -18,21 +34,34 @@
 
     </div>
 
-    <div class="slideshow"></div>
+    <style type="text/css"><?php print $slideShowCSS ?></style>
+    <div class="gallery autoplay items-5">
+        <div class="item gallery-image-1"></div>
+        <div class="item gallery-image-2"></div>
+        <div class="item gallery-image-3"></div>
+        <div class="item gallery-image-4"></div>
+        <div class="item gallery-image-5"></div>
+    </div>
 
     <div class="footer">
         <ul class="tiles">
             <li>
                 <img src="./images/collections/15683460864_9091c86743_q.jpg"/>
-                <p class="title">concert</p>
+                <span class="caption simple-caption">
+                    <p>tiles 1</p>
+                </span>
             </li>
             <li>
                 <img src="./images/collections/16133722758_53ccf0279f_q.jpg"/>
-                <p class="title">fashion</p>
+                <span class="caption simple-caption">
+                    <p>tiles 2</p>
+                </span>
             </li>
             <li>
                 <img src="./images/collections/16892810316_0eaa62f27d_q.jpg"/>
-                <p class="title">studio</p>
+                <span class="caption simple-caption">
+                    <p>tiles 3</p>
+                </span>
             </li>
         </ul>
     </div>
